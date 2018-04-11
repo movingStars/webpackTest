b7963c5e78029699b5b702287aa6b3579ea9843f
movingStars <1694056853@qq.com>
2018-04-11 18:29:44 +0800

    '引入一些插件'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+import printMe from './print.js';
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
-    element.innerHTML = "Hello world!";
+    element.innerHTML = "今日头条";
+    btn.innerHTML = "点我点我";
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
 }
---
+++ src/print.js
@@ -0,0 +1,6 @@
+export function printMe(){
+    console.log("123123");
+}
+export function tellMe(){
+    alert("ABCDEFG");
+}
